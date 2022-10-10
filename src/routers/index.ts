e2534e409606a7f7321373a401c1d6b60990d14d
Isabela <isabela_cmoreira@hotmail.com>
2022-10-10 13:34:11 -0300

    feat: added the nail polish router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import authRouter from "./authRouter";
+import nailPolishRouter from "./nailPolishRouter";
 
 const router = Router();
 router.use(authRouter);
+router.use(nailPolishRouter);
 export default router;
