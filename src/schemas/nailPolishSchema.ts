3eaca413350f5a80a7976ae526042f84894feaec
Isabela <isabela_cmoreira@hotmail.com>
2022-10-10 16:43:30 -0300

    feat: added nail polish schema

---
+++ src/schemas/nailPolishSchema.ts
@@ -0,0 +1,13 @@
+import joi from "joi";
+import { NailPolishData } from "../types/NailPolishDataType";
+
+export const nailPolishSchema = joi.object<NailPolishData>({
+  name: joi.string().min(2).required().label("Name is a required field"),
+  brand: joi.string().min(2).required().label("Brand is a required field"),
+  shade: joi.string().min(2).required().label("Shade is a required field"),
+  finish: joi.string().min(2).required().label("Finish is a required field"),
+  expirationDate: joi
+    .date()
+    .required()
+    .label("Expiration date is a required field"),
+});
