6088ded0a9942c71e21eb52c17306f9c6cc59ad9
Isabela <isabela_cmoreira@hotmail.com>
2023-04-04 17:06:49 -0300

    feat: added a seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,109 +1,96 @@
-import { prisma } from "../src/config/database";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 async function main() {
-  const users = [
-    {
-      name: "Isabela",
-      lastName: "Moreira",
-      email: "isabela_moreira@hotmail.com",
-      password: "soidolosflutime",
-      confirmPassword: "soidolosflutime",
-    },
-    {
-      name: "Dengo",
-      lastName: "Moreira",
-      email: "dengo_gato@gmail.com",
-      password: "timedeguerreiros",
-      confirmPassword: "timedeguerreiros",
-    },
-    {
-      name: "Dara",
-      lastName: "Moreira",
-      email: "dara_gata@gmail.com",
-      password: "luideverso22",
-      confirmPassword: "luideverso22",
-    },
-  ];
-  users.forEach(async (item) => {
-    await prisma.user.upsert({
-      where: { email: item.email },
-      update: {},
-      create: item,
-    });
+  await prisma.user.createMany({
+    data: [
+      {
+        name: "Isabela",
+        last_name: "Moreira",
+        email: "isabelamoreira@hotmail.com",
+        password: "soidolosflutime",
+      },
+      {
+        name: "Dengo",
+        last_name: "Moreira",
+        email: "dengo_gato@gmail.com",
+        password: "timedeguerreiros",
+      },
+      {
+        name: "Dara",
+        last_name: "Moreira",
+        email: "dara_gata@gmail.com",
+        password: "luideverso22",
+      },
+    ],
   });
+  await prisma.nailPolish.createMany({
+    skipDuplicates: true,
+    data: [
+      {
+        name: "Jackie",
+        brand: "Impala",
+        shade: "Brown",
+        finish: "Crème",
+        expiration_date: "2022-11-22",
+      },
+      {
+        name: "Cherries in the snow",
+        brand: "Revlon",
+        shade: "Pink",
+        finish: "Crème",
+        expiration_date: "2025-02-04",
+      },
+      {
+        name: "In a While Crocodile",
+        brand: "Essence",
+        shade: "Green",
+        finish: "Crème",
+        expiration_date: "2024-07-20",
+      },
+      {
+        name: "Malibu Marigolden Hour",
+        brand: "OPI",
+        shade: "Yellow",
+        finish: "Crème",
+        expiration_date: "2023-03-27",
+      },
+      {
+        name: "Na Boca do Povo",
+        brand: "Dailus",
+        shade: "Pink and silver",
+        finish: "Glitter",
+        expiration_date: "2023-01-31",
+      },
+      {
+        name: "Cetim dourado",
+        brand: "Risqué Diamond Gel",
+        shade: "Gold",
+        finish: "Metallic",
+        expiration_date: "2025-06-22",
+      },
+      {
+        name: "Sweet Grape",
+        brand: "Dailus",
+        shade: "Purple",
+        finish: "Jelly",
+        expiration_date: "2022-07-14",
+      },
 
-  const nailPolishes = [
-    {
-      name: "Jackie",
-      brand: "Impala",
-      shade: "Brown",
-      finish: "Crème",
-      expirationDate: "2022-11-22",
-    },
-    {
-      name: "Cherries in the snow",
-      brand: "Revlon",
-      shade: "Pink",
-      finish: "Crème",
-      expirationDate: "2025-02-04",
-    },
-    {
-      name: "In a While Crocodile",
-      brand: "Essence",
-      shade: "Green",
-      finish: "Crème",
-      expirationDate: "2024-07-20",
-    },
-    {
-      name: "Malibu Marigolden Hour",
-      brand: "OPI",
-      shade: "Yellow",
-      finish: "Crème",
-      expirationDate: "2023-03-27",
-    },
-    {
-      name: "Na Boca do Povo",
-      brand: "Dailus",
-      shade: "Pink and silver",
-      finish: "Glitter",
-      expirationDate: "2023-01-31",
-    },
-    {
-      name: "Cetim dourado",
-      brand: "Risqué Diamond Gel",
-      shade: "Gold",
-      finish: "Metallic",
-      expirationDate: "2025-06-22",
-    },
-    {
-      name: "Sweet Grape",
-      brand: "Dailus",
-      shade: "Purple",
-      finish: "Jelly",
-      expirationDate: "2022-07-14",
-    },
-
-    {
-      name: "Condessa",
-      brand: "Risqué",
-      shade: "Pink Nude",
-      finish: "Sheer",
-      expirationDate: "2024-08-14",
-    },
-  ];
-
-  nailPolishes.forEach(async (item) => {
-    await prisma.nailPolish.upsert({
-      where: { name: item.name },
-      update: {},
-      create: item,
-    });
+      {
+        name: "Condessa",
+        brand: "Risqué",
+        shade: "Pink Nude",
+        finish: "Sheer",
+        expiration_date: "2024-08-14",
+      },
+    ],
   });
 }
 
 main()
   .catch((e) => {
-    console.log(e);
+    console.error(e);
     process.exit(1);
   })
   .finally(async () => {
